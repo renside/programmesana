f3e866e7a364214eff2270a4bedf6e3f9e5ecee0
renside <renside21@gmail.com>
2024-04-09 14:01:59 +0300

    progra

---
+++ templates/trenins.py
@@ -0,0 +1,19 @@
+def summa(sk1, sk2):
+    sum = sk1+sk2
+    return sum
+
+skaitlis1 = 4
+skaitlis2 = 3
+
+print("pirmais skaitlis:", skaitlis1)
+print("otrais skaitlis:", skaitlis2)
+print("summa:", summa(skaitlis1,skaitlis2))
+
+pirmais = ("pirmais skaitlis:", skaitlis1, "otrais skaitlis", skaitlis2, "summa:", summa(skaitlis1,skaitlis2))
+
+print = "6"
+print(pirmais)
+
+vards2 = "Nē"
+
+print(pirmais, vards2)
